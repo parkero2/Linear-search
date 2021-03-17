33a7cceae99ef0019f1d8410c750a95d5c42e6d7
parkero2 <parkero2@stu.otc.school.nz>
2021-03-18 09:14:01 +1300

    Randomiser

--- search.py
+++ search.py
@@ -1,7 +1,15 @@
 numbers = []
+import random
 
 for i in range(int(input("Set a list length"))):
     numbers.append(i+1)
+
+for z in range(len(numbers)):
+  randintt = random.randint(0, len(numbers)-1)
+  yemp = numbers[z]
+  numbers[z] = numbers[randintt]
+  numbers[randintt] = yemp
+
 print(numbers) #The list of numbers
 
 find_num = int(input("What number to find?"))
